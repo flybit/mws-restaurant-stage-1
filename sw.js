6ab2691af5ed7048c7c76fd3f002ebe7b5003d65
flybit <alphan@bu.edu>
2018-09-06 17:54:57 -0400

    Add files to cache to service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,24 @@
 var CACHE_NAME = 'udacity-restaurant-reviews-v1';
+var FILES_TO_CACHE = [
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/10.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/index.html',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/restaurant.html',
+    '/sw.js'
+];
 
 // Always try to fetch from network and put the response to cache on success.
 // If this fails, return previously cached data, if any. Otherwise, respond with error.
